eaa3835ed6a6abcbb50486deadc69b892861b13c
Houda <houda.bentaleb@gmail.com>
2025-12-05 21:49:41 +0100

    19.0[MIG]partner_tier_validation

---
+++ partner_tier_validation/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Scalizer (<https://www.scalizer.fr>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Partner Tier Validation",
+    "summary": "Support a tier validation process for Contacts",
+    "version": "19.0.1.0.0",
+    "website": "https://github.com/ScalizerOrg/partner-contact",
+    "category": "Contact",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["contacts", "base_tier_validation", "partner_stage"],
+    "data": [
+        "data/tier_definition.xml",
+        "views/res_partner_view.xml",
+    ],
+    "maintainers": ["Scalizer"],
+}
